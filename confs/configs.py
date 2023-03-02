3f98e9629908cc3994426d1ef7407060d33b4177
AzizNadirov <eziznadirov@gmail.com>
2023-03-02 23:48:16 +0400

    fixed BASE_DIR bug

--- confs/configs.py
+++ confs/configs.py
@@ -1,8 +1,12 @@
 #!Python 3.9.16
 # config constants
+from pathlib import Path
 import os
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath('__file__'))))
+BASE_DIR = Path(os.path.abspath(os.path.dirname(__file__))).parent
+
 BANK_DIR = f"{BASE_DIR}/confs/ebs"
 
 TEMPLATES_DIR = f"{BASE_DIR}/src/templates"
+
+
